2a8f31f238eea02667c06454c7fcc75e3c10d86c
David Nolen <david.nolen@gmail.com>
2009-09-03 23:38:36 -0400

    Initial commit.

---
+++ FuncTools.js
@@ -0,0 +1,181 @@
+function $identity(v) { return v; }
+
+function $callable(v) { return v && $type(v) == 'function'; }
+
+function $not(fn) {
+  return function() {
+    return !fn.apply(this, $A(arguments));
+  }
+}
+
+function $isnull(v) { return v === null; };
+
+function $notnull(v) { return v !== null; };
+
+function $iterate(n, fn) {
+  var result = [];
+  (n).times(function() {
+    result.push(fn());
+  });
+  return result;
+};
+
+function $repeat(n, v) {
+  return $iterate(n, $lambda(v));
+};
+
+function $arglist(fn) {
+  return fn.toString().match(/function \S*\((.*?)\)/)[1].split(',');
+};
+
+function $arity() {
+  var fns = $A(arguments);
+  var dispatch = [];
+  fns.each(function(fn) {
+    var arglist = $arglist(fn);
+    dispatch[arglist.length] = fn;
+  });
+  return function () {
+    var args = $A(arguments).filter($notnull);
+    return dispatch[args.length].apply(this, args);
+  }
+};
+
+var sum = $arity(
+  function(a) { return a; },
+  function(a, b) { return a + (($type(b) == 'array') ? b.first() || 0 : b); }
+);
+
+function $reduce(fn, ary) {
+  ary = $A(ary);
+  var result = ary.first();
+  while(ary.length != 0) {
+    var rest = ary.rest();
+    result = fn(result, rest);
+    ary = rest;
+  }
+  return result;
+};
+
+function $get(first, prop) {
+  var args = $A(arguments), rest = args.rest(2), next;
+  if(rest.length == 0) return first[prop];
+  if(['object', 'array'].contains($type(first))) next = first[prop];
+  if($type(next) == 'function') next = first[prop]();
+  return (next == null) ? null : $get.apply(null, [next].concat(rest));
+};
+
+function $acc() {
+  var args = $A(arguments);
+  return function(obj) {
+    return $get.apply(null, [obj].combine(args));
+  };
+};
+
+Function.implement({
+  decorate: function() {
+    var decorators = $A(arguments), orig = resultFn = this, decorator;
+    while(decorator = decorators.pop()) resultFn = decorator(resultFn);
+    resultFn._arglist = $arglist(orig);
+    resultFn._decorated = orig;
+    return resultFn;
+  },
+
+  comp: function() {
+    var fns = $A(arguments), self = this;
+    return function() {
+      var temp = $A(fns);
+      var args = $A(arguments), result = (self && $type(self) == 'function') ? self.apply(this, args) : null, fn;
+      while(fn = temp.shift()) result = fn.apply(null, (result && [result]) || args);
+      return result;
+    }
+  },
+  
+  partial: function(bind, args) {
+    var self = this;
+    args = $splat(args);
+    return function() {
+      return self.apply(bind, args.concat($A(arguments)));
+    };
+  }
+});
+var $comp = Function.comp;
+
+/*
+  Decorator: memoize
+    Because lazy-loading of resources is so common, I've included
+    memoize. This will memoize the return values of a function depending
+    on the arguments passed in. Note that if you call a function frequently
+    with many different kinds of arguments you will consume memory very
+    quickly. This decorator works best with arguments not containing any
+    object values. This is becuase the args array is JSON encoded into a
+    string for comparison. So arguments composed of strings, integers, and 
+    arrays work best. Normally this would be a problem but for acecssing remote 
+    resources this limitation is fine.
+*/
+function memoize(fn) {
+  var table = {};
+  return function memoized() {
+    var args = $A(arguments);
+    var enc = JSON.encode(args);
+    if(!table[enc]) {
+      var result = fn.apply(this, args);
+      table[enc] = result;
+      return result;
+    } else {
+      return table[enc];
+    }
+  };
+}
+
+// We need a backreference to wrapper to support decorator usage from within classes - David
+Class.extend({
+  wrap: function(self, key, method) {
+    if (method._origin) method = method._origin;
+    var wrapper = function() {
+      if (method._protected && this._current == null) throw new Error('The method "' + key + '" cannot be called.');
+      var caller = this.caller, current = this._current;
+      this.caller = current; this._current = arguments.callee;
+      var result = method.apply(this, arguments);
+      this._current = current; this.caller = caller;
+      return result;
+    }.extend({_owner: self, _origin: method, _name: key});
+    method._wrapper = wrapper;
+    return wrapper;
+  }
+});
+
+Array.implement({
+  first: function() { return this[0]; },
+  rest: function(n) { return this.slice(n || 1, this.length); },
+  drop: function(n) { return this.slice(0, this.length-n); },
+  tail: function(n) { return this.slice(n, this.length); },
+  head: function(n) { return this.slice(0, n) },
+  partition: function(n) {
+    if(this.length % n != 0) throw Error("The length of this array is not a multiple of " + n);
+    var result = [];
+    var ary = this;
+    while(ary.length > 0) {
+      var sub = ary.head(n);
+      result.push(sub);
+      ary = ary.tail(n);
+    }
+    return result;
+  }
+});
+
+Hash.implement({
+  asFn: function() {
+    var self = this;
+    return function(k) {
+      return self[k];
+    };
+  }
+})
+
+function $msg(methodName) {
+  var rest = $A(arguments).rest();
+  return function(obj) {
+    return obj[methodName].apply(obj, rest);
+  };
+}
